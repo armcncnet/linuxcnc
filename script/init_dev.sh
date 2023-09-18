0faed3eafa4dbafd84a161d6875cb3277243dd42
GeekROS <15300555770>
2023-09-18 22:07:24 +0800

    1.0.0

---
+++ script/init_dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+uname -a
+
+lsb_release -a
+
+architecture=$(dpkg --print-architecture)
+
+if [ ! -d "/usr/local/go/bin/" ]; then
+    sudo wget -q https://studygolang.com/dl/golang/go1.19.4.linux-"${architecture}".tar.gz && sudo tar -C /usr/local -xzf go1.19.4.linux-"${architecture}".tar.gz && sudo rm -rf go1.19.4.linux-"${architecture}".tar.gz
+    sudo sh -c 'echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile'
+    # shellcheck disable=SC1090
+    source /etc/profile && source ~/.bashrc
+fi
+
+echo "Install development environment successfully"
