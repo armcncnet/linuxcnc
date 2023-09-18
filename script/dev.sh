535c1944e186ca797e9e998650c2f5b525d59007
GeekROS <15300555770>
2023-09-18 22:20:59 +0800

    1.0.0

--- script/dev.sh
+++ script/dev.sh
@@ -8,6 +8,8 @@ lsb_release -a
 
 architecture=$(dpkg --print-architecture)
 
+sudo apt install -y nodejs yarn
+
 if [ ! -d "/usr/local/go/bin/" ]; then
     sudo wget -q https://studygolang.com/dl/golang/go1.19.4.linux-"${architecture}".tar.gz && sudo tar -C /usr/local -xzf go1.19.4.linux-"${architecture}".tar.gz && sudo rm -rf go1.19.4.linux-"${architecture}".tar.gz
     sudo sh -c 'echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile'
