b06f31276d913e71396c00d3083d41bbbca27350
GeekROS <15300555770>
2023-09-18 22:14:03 +0800

    1.0.0

---
+++ script/publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+version=$1
+# shellcheck disable=SC2037
+architecture=$(dpkg --print-architecture)
+
+cd ../ && echo "$PWD $architecture $version"
+lsb_release -a
+
+sudo apt install -y dpkg-dev gpg
+
+export GO111MODULE=on && export GOPROXY=https://goproxy.io && sudo rm -rf main
+sudo -E /usr/local/go/bin/go build main.go
+sudo cp main debian/usr/local/bin/robotchain
+sudo rm -rf main
+
+echo "Publish successfully"
