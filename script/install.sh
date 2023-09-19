a2944f4100aa402bb69ee655d7eb7d0812f9efd4
GeekROS <15300555770>
2023-09-19 12:48:36 +0800

    1.0.0

--- script/install.sh
+++ script/install.sh
@@ -13,7 +13,7 @@ if [ ! -f "/etc/apt/sources.list.d/armcnc.list" ]; then
     sudo wget -q -O /tmp/Release.gpg https://mirrors.geekros.com/dists/focal/Release.public
     # shellcheck disable=SC2024
     sudo gpg --dearmor < /tmp/Release.gpg > /etc/apt/trusted.gpg.d/geekros-archive.gpg
-    echo "deb [arch=${architecture}] https://mirrors.geekros.com/ bookworm main contrib non-free non-free-firmware" | sudo tee /etc/apt/sources.list.d/armcnc.list
+    echo "deb [arch=${architecture}] https://mirrors.geekros.com/ focal main" | sudo tee /etc/apt/sources.list.d/armcnc.list
 fi
 
 sudo apt -y update && sudo apt -y upgrade
