ff5c1675e43a4a9281e5e7bc0ba7e78d49fc3b7d
GeekROS <15300555770>
2023-09-20 09:01:11 +0800

    1.0.0

--- debian/opt/armcnc/python/launch.py
+++ debian/opt/armcnc/python/launch.py
@@ -3,9 +3,12 @@
 
 import armcnc as framework
 
-def framework_start(cnc):
+def armcnc_start(cnc):
     while True:
         pass
+
+def armcnc_exit(cnc):
+    pass
 
 if __name__ == '__main__':
     framework_sdk = framework.Init()
