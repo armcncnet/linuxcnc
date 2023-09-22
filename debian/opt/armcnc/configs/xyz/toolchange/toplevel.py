46f9664a2c066abf3c018d3bf4fb205baf9656fe
GeekROS <15300555770>
2023-09-22 09:40:52 +0800

    1.0.0

--- debian/opt/armcnc/configs/xyz/toolchange/toplevel.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#from linuxcnc import INTERP_EXECUTE_FINISH, INTERP_OK
-import remap
-
-def __init__(self,**words):
-    print len(words)," words passed"
-    for w in words:
-        print "%s: %s" % (w, words[w])
