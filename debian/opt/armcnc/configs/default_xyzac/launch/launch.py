b0e4cf833f645908b13b83f9d565abb8507720d5
GeekROS <15300555770>
2023-10-06 16:35:31 +0800

    1.0.0

---
+++ debian/opt/armcnc/configs/default_xyzac/launch/launch.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import armcnc as framework
+
+def armcnc_start(cnc):
+    while True:
+        pass
+
+def armcnc_message(cnc, message):
+    pass
+
+def armcnc_exit(cnc):
+    pass
+
+if __name__ == '__main__':
+    framework_sdk = framework.Init()
