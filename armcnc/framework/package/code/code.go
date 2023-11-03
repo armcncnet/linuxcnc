1965d66e6bf03ac4ee76912f904754d33a805d23
GeekROS <15300555770>
2023-11-03 10:24:46 +0800

    1.0.0

--- armcnc/framework/package/code/code.go
+++ armcnc/framework/package/code/code.go
@@ -9,7 +9,9 @@ package CodePackage
 
 import (
 	"armcnc/framework/config"
+	"bufio"
 	"github.com/djherbis/times"
+	"github.com/goccy/go-json"
 	"os"
 	"strings"
 	"time"
@@ -50,7 +52,42 @@ func (code *Code) Select() []Data {
 			if strings.Contains(file.Name(), "demo") {
 				item.Time = item.Time.Add(-10 * time.Minute)
 			}
+			firstLine := code.ReadFirstLine(code.Path + file.Name())
+			if firstLine.Version != "" {
+				item.Name = firstLine.Name
+				item.Describe = firstLine.Describe
+				item.Version = firstLine.Version
+				data = append(data, item)
+			}
 		}
+	}
+
+	return data
+}
+
+func (code *Code) ReadFirstLine(path string) Data {
+	data := Data{}
+	file, err := os.Open(path)
+	if err != nil {
+		return data
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return data
+	}
+
+	line = strings.TrimSpace(line)
+
+	if len(line) > 0 && line[0] == '(' && line[len(line)-1] == ')' {
+		jsonStr := line[1 : len(line)-1]
+		err := json.Unmarshal([]byte(jsonStr), &data)
+		if err != nil {
+			return data
+		}
+
 	}
 
 	return data
