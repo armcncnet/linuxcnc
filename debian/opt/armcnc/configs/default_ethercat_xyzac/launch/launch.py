60c316ae2e8e32f062f722640d430c053907bce7
GeekROS <15300555770>
2023-11-30 14:28:49 +0800

    1.0.0

---
+++ debian/opt/armcnc/configs/default_ethercat_xyzac/launch/launch.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import armcnc as sdk
+
+def armcnc_start(cnc):
+    while True:
+        pass
+
+def armcnc_message(cnc, message):
+    pass
+
+def armcnc_exit(cnc):
+    pass
+
+if __name__ == '__main__':
+    sdk.Init()
