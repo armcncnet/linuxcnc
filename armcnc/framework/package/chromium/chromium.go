c7a35157e0f6d9c160bcfcc05375a3f6e586423f
GeekROS <15300555770>
2023-12-13 10:24:34 +0800

    1.0.0

--- armcnc/framework/package/chromium/chromium.go
+++ armcnc/framework/package/chromium/chromium.go
@@ -7,7 +7,10 @@
 
 package ChromiumPackage
 
-import "armcnc/framework/config"
+import (
+	"armcnc/framework/config"
+	"os/exec"
+)
 
 type Chromium struct {
 	Path string `json:"path"`
@@ -20,4 +23,19 @@ func Init() *Chromium {
 	return &Chromium{
 		Path: Config.Get.Basic.Workspace + "/",
 	}
+}
+
+func (chromium *Chromium) OnStart() {
+	cmd := exec.Command("systemctl", "start", "armcnc_chromium.service")
+	cmd.Output()
+}
+
+func (chromium *Chromium) OnReStart() {
+	cmd := exec.Command("systemctl", "restart", "armcnc_chromium.service")
+	cmd.Output()
+}
+
+func (chromium *Chromium) OnStop() {
+	cmd := exec.Command("systemctl", "stop", "armcnc_chromium.service")
+	cmd.Output()
 }
