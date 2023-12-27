9ce6de2e78e15e7ab4a9023a6aafe13ed3f429ee
GeekROS <15300555770>
2023-12-27 18:07:08 +0800

    1.0.0

--- armcnc/framework/package/launch/launch.go
+++ armcnc/framework/package/launch/launch.go
@@ -49,7 +49,7 @@ func (launch *Launch) OnStart() {
 		launch.OnRemoveTmp()
 		cmd := exec.Command("systemctl", "start", "armcnc_linuxcnc.service")
 		cmd.Output()
-		time.Sleep(1 * time.Second)
+		time.Sleep(2 * time.Second)
 		cmd = exec.Command("systemctl", "start", "armcnc_launch.service")
 		cmd.Output()
 		go func() {
@@ -68,7 +68,7 @@ func (launch *Launch) OnRestart() {
 	launch.OnRemoveTmp()
 	cmd := exec.Command("systemctl", "restart", "armcnc_linuxcnc.service")
 	cmd.Output()
-	time.Sleep(1 * time.Second)
+	time.Sleep(2 * time.Second)
 	cmd = exec.Command("systemctl", "restart", "armcnc_launch.service")
 	cmd.Output()
 }
